7be62930b4d4f50b6cd87b28cfed620869ce2d99
Cin💜 Lo <cinlos3382@gmail.com>
2023-03-17 11:42:07 -0300

    se crea el layout y se agregan el header y nav; tambien agregamos las rutas en jsconfig.json

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
+      <header className="absolute inset-x-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="/login" className="-m-1.5 p-1.5">
